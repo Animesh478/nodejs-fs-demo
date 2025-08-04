14e70a7e90d9bfee09717f1de2a9150fb5ff53b8
Animesh478 <animeshmohanty.am@gmail.com>
2025-08-04 17:27:56 +0530

    moved the routing logic to another file

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
 const http = require("http");
-const fs = require("fs");
+const routeHandler = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.setHeader("content-type", "text/html");
-    fs.readFile("message.txt", (err, data) => {
-      res.end(`
-            <p>${data}</p>
-            <form action="/writeFile" method="post">
-                <input type="text" name="username"/>
-                <button type="submit">Send</button>
-            </form>
-            `);
-    });
-  } else {
-    if (url === "/writeFile") {
-      const data = [];
-      req.on("data", (chunk) => {
-        data.push(chunk);
-      });
-
-      req.on("end", () => {
-        const formData = Buffer.concat(data).toString();
-        const formValues = formData.split("=")[1];
-
-        fs.appendFile("message.txt", `${formValues}\n`, (err) => {
-          res.statusCode = 302;
-          res.setHeader("Location", "/");
-          res.end();
-        });
-      });
-    }
-  }
-});
+const server = http.createServer(routeHandler);
 
 server.listen(3000, () => {
   console.log("Server is running");
---
+++ routes.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+function routeHandler(req, res) {
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("content-type", "text/html");
+    fs.readFile("message.txt", (err, data) => {
+      res.end(`
+            <p>${data}</p>
+            <form action="/writeFile" method="post">
+                <input type="text" name="username"/>
+                <button type="submit">Send</button>
+            </form>
+            `);
+    });
+  } else {
+    if (url === "/writeFile") {
+      const data = [];
+      req.on("data", (chunk) => {
+        data.push(chunk);
+      });
+
+      req.on("end", () => {
+        const formData = Buffer.concat(data).toString();
+        const formValues = formData.split("=")[1];
+
+        fs.appendFile("message.txt", `${formValues}\n`, (err) => {
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          res.end();
+        });
+      });
+    }
+  }
+}
+
+module.exports = routeHandler;
